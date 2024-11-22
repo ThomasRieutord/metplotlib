137c4de889ae8cb779f760b68d4ce250dd9b9e24
Thomas Rieutord <thomas.rieutord@gmail.com>
2024-11-22 14:07:12 +0000

    Modernize installation procedure

--- metplotlib/__init__.py
+++ metplotlib/__init__.py
@@ -8,9 +8,9 @@ import os
 
 package_rootdir = os.path.dirname(os.path.realpath(__path__[0]))
 
-with open(os.path.join(package_rootdir, "setup.py"), "r") as f:
+with open(os.path.join(package_rootdir, "pyproject.toml"), "r") as f:
     for l in f.readlines():
-        if "version=" in l:
+        if "version =" in l:
             __version__ = l.split('"')[1]
             break
 
